7ccd029ce7568a92b66bd642c25845ca5acb805c
bhagyahelawar7-jpg <bhagyahelawar7@gmail.com>
2026-08-25 20:29:49 +0530

    Array_of_object

---
+++ Array_of_object.java
@@ -0,0 +1,15 @@
+public class Array_of_object{
+    public static void main(String[]args){
+        int nums[] = new int[9];
+         nums[0] = 5;
+         nums[1] = 4;
+         nums[2] = 7;
+         nums[3] = 6;
+         nums[4] = 2;
+         nums[5] = 3;
+
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
